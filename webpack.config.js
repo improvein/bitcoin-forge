7de79d370619299541655c4d856f4fc1ca17d704
diegogurpegui <diego.gurpegui@improvein.com>
2019-02-03 21:46:33 -0300

    New dist build

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '/',
+    publicPath: '/bitcoin-forge',
     filename: '[name].[contenthash:8].js',
   },
   optimization: {
