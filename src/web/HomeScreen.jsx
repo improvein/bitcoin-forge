068e90ff5921bf7a4dc18cfd6d692973f83415a5
diegogurpegui <diego.gurpegui@improvein.com>
2019-06-09 19:41:05 -0300

    Home screen proyect description

--- src/web/HomeScreen.jsx
+++ src/web/HomeScreen.jsx
@@ -7,6 +7,28 @@ const HomeScreen = () => (
       This project intends to be a suite of tools for low-level operations with Bitcoin, like
       forging transactions or tinker with scripts.
     </p>
+    <h2>Features</h2>
+    <p>These are some of the things you can do with <strong>Bitcoin Forge</strong>:</p>
+    <ul>
+      <li>Transactions
+        <ul>
+          <li>Create a transaction by defining the inputs and outputs specifically</li>
+          <li>Deconde a transaction from it's raw hexa</li>
+        </ul>
+      </li>
+      <li>Address
+        <ul>
+          <li>Create an address in different formats given the public key(s) or redeem script</li>
+        </ul>
+      </li>
+      <li>Scripts
+        <ul>
+          <li>Decompile the script ASM given the raw hexa notation</li>
+          <li>Create a script manually by adding values or OPCODES to the stack</li>
+        </ul>
+      </li>
+    </ul>
+    <h2>Development notes</h2>
     <p>
       This web front-end is developed using <a href="https://reactjs.org">React</a>
       {', '}
