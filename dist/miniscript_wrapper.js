d36c5207bfd5da596f12ec3ed159bf2a221b44f6
Diego H. Gurpegui <diego.gurpegui@improvein.com>
2020-12-20 20:44:52 -0300

    Version bump and build

--- dist/miniscript_wrapper.js
+++ dist/miniscript_wrapper.js
@@ -1,17 +1,1 @@
-/* global Module */
-em_miniscript_compile = Module.cwrap('miniscript_compile', 'none', [
-  'string',
-  'number',
-  'number',
-  'number',
-  'number',
-  'number',
-  'number',
-]);
-em_miniscript_analyze = Module.cwrap('miniscript_analyze', 'none', [
-  'string',
-  'number',
-  'number',
-  'number',
-  'number',
-]);
+em_miniscript_compile=Module.cwrap("miniscript_compile","none",["string","number","number","number","number","number","number"]),em_miniscript_analyze=Module.cwrap("miniscript_analyze","none",["string","number","number","number","number"]);
